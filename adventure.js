d198ec6cfcc6ee6d8d7f16918b26ebc0cb51606e
Mikael Engström <micke@primat.se>
2023-04-06 12:54:40 +0200

    Initial commit

---
+++ adventure.js
@@ -0,0 +1,6 @@
+const adventure = [
+    {name:"Start", type:"ok", text: "Nu börjar det!", lat: 59.32279544524491, lon: 17.99007936691471 },
+    {name:"Punkt 1", type:"ok", text: "Du hittade punkt 1", lat: 17.99008596255015, lon: 59.32288546150653},
+    {name:"Punkt 2", type:"ok", text: "Du hittade punkt 2", lat: 17.990179950355135, lon: 59.32284508040931}
+]
+
---
+++ script.js
@@ -0,0 +1,72 @@
+setInterval(updateDistance, 2000)
+let playerLat, playerLon, playerDistance, tLat, tLon
+
+let gamePosition = 0;
+
+nextPlace()
+
+function nextPlace()
+{
+    tLat = adventure[gamePosition].lat
+    tLon = adventure[gamePosition].lon
+    console.log(tLat, tLon)
+}
+
+
+function updateDistance()
+{
+
+
+
+    let d = document.getElementById("distance")
+
+
+    if (navigator.geolocation) {
+
+    // Den här metoden returnerar ett Position-objekt
+    // Sedan körs getPostion, som tar med sig Postion som inparameter
+    navigator.geolocation.getCurrentPosition(getPosition)
+    }
+    else
+    {
+        d.innerHTML = "Supportas inte"
+    }
+
+
+
+
+    d.innerHTML = playerDistance
+
+    document.getElementById("lat").innerHTML = playerLat
+    document.getElementById("lon").innerHTML = playerLon
+}
+
+
+let places = ""
+
+
+
+function getPosition(position)
+{
+    playerLat = position.coords.latitude
+    playerLon =  position.coords.longitude
+    playerDistance = distance(playerLat, playerLon, tLat, tLon, "M")
+}
+
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    if (unit=="M") { dist = dist * 1609.344; dist = Math.round(dist)}
+    console.log(dist)
+    return dist
+}
