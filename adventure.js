0ae2b672c44a13b7f5439c522e2a687e52c4002b
Mikael Engström <micke@primat.se>
2023-04-06 14:54:24 +0200

    Update adventure.js

--- adventure.js
+++ adventure.js
@@ -1,6 +1,19 @@
 const adventure = [
-    {name:"Start", type:"ok", text: "Nu börjar det!", clue: "", lat: 59.32279544524491, lon: 17.99007936691471 },
-    {name:"Köket", type:"ok", text: "Cool plats 1", clue: "Gå till köket", lat: 59.32279544524491, lon: 17.99007936691471 },
+    {   name:"Start", 
+        type:"ok", 
+        text: "Nu börjar det!", 
+        clue: "", 
+        lat: 59.32279544524491, 
+        lon: 17.99007936691471 
+    },
+    {
+        name:"Köket", 
+        type:"ok", 
+        text: "Cool plats 1", 
+        clue: "Gå till köket", 
+        lat: 59.32279544524491, 
+        lon: 17.99007936691471 
+    },
     {name:"Punkt 1", type:"ok", text: "Du hittade punkt 1",clue: "Leta i köket", lat: 17.99008596255015, lon: 59.32288546150653},
     {name:"Punkt 2", type:"ok", text: "Du hittade punkt 2",clue: "Leta i balkongrummet", lat: 17.990179950355135, lon: 59.32284508040931}
 ]
